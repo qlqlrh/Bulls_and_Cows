ef7e2205ef955ed2be705b8b1fbe8a2125398144
qlqlrh <dongin@pusan.ac.kr>
2023-05-21 23:50:57 +0900

    using srand, time

--- print.c
+++ print.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 int main( void )
 {
-	int number;
+	int input_num;
+	int random_num;
+	srand(time(NULL));	
+	random_num = rand() % 1000;
+
 	printf("Input 3 digits number : ");
-	scanf("%d", &number);
-	printf("Your number is %d\n", number);
+	scanf("%d", &input_num);
+	
+	printf("Your number is %d\n", input_num);
+	printf("Random num is %d\n", random_num);
+	
 	return 0;
 }
