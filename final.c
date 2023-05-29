ffb1ed7236a67457a501abb7bb410601180495e9
qlqlrh <dongin@pusan.ac.kr>
2023-05-29 15:54:17 +0900

    final version

---
+++ final.c
@@ -0,0 +1,66 @@
+// Bulls and Cows (aka.Number baseball)
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main( void )
+{
+    srand(time(NULL));
+    
+    int input_num = 0;
+    int user[3], computer[3];
+    int ball = 0, strike = 0, nothing = 0;
+    
+    for (int i = 0; i < 3; i++) {
+        int random_num;
+        int isUnique;
+        
+        do {
+            isUnique = 1;
+            random_num = rand() % 10;
+            
+            for (int j = 0; j < i; j++) {
+                if (computer[j] == random_num) {
+                    isUnique = 0;
+                    break;
+                }
+            }
+        } while (!isUnique); // isNotUniq이면 계속 랜덤 생성 해야 함
+        
+        computer[i] = random_num;
+
+    }
+    
+    for (int k = 0; k < 10; k++) {
+        printf("%d. Input 3 digits number : ", k+1);
+        scanf("%d", &input_num);
+        
+        int n = 100;
+        for (int i = 0; i < 3; i++) {
+            user[i] = input_num / n % 10;
+            n /= 10;
+        }
+        
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (user[i] == computer[j]) {
+                    if (i == j)
+                        strike += 1;
+                    else
+                        ball += 1;
+                }   
+            }
+        }
+        nothing = 3 - strike - ball;
+        printf("Strike: %d, Ball: %d, Nothing = %d\n", strike, ball, nothing);
+        if (strike == 3) {
+            printf("Correct! Random num is %d%d%d\n", computer[0], computer[1], computer[2]);
+            break;
+        }
+        strike = 0, ball = 0, nothing = 0;
+    }
+    
+    if (strike != 3)
+        printf("Sorry, Random num is %d%d%d\n", computer[0], computer[1], computer[2]);
+    return 0;
+}
